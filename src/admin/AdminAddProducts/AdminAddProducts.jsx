69963f91a798c120622e235dd408660d5b029fd2
SajiburMunna <sajiburmunna087@gmail.com>
2021-12-06 02:06:48 +0600

    admin dashboard with add products,categories,home page design

--- src/admin/AdminAddProducts/AdminAddProducts.jsx
+++ src/admin/AdminAddProducts/AdminAddProducts.jsx
@@ -1,10 +1,90 @@
 import React from "react";
 import "./AdminAddProducts.css";
+import Box from "@mui/material/Box";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const AdminAddProducts = () => {
+  const [category, setCategory] = React.useState("");
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
   return (
     <div>
-      <h1>Add products</h1>
+      <h1 className="admin-addproducts-headline ">Add products</h1>
+      <form action="">
+        <div style={{ textAlign: "center" }}>
+          <input
+            style={{ width: "550px", marginBottom: "20px" }}
+            type="text"
+            placeholder="Products Title"
+            required
+          />
+          <div className="admin-addproducts-file-categories-div">
+            <div>
+              <input
+                style={{ width: "300px" }}
+                type="file"
+                id="myfile"
+                name="myfile"
+                required
+              ></input>
+            </div>
+            <div>
+              <Box style={{ width: "200px", marginLeft: "50px" }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    category
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={category}
+                    label=" category"
+                    onChange={handleChange}
+                    required
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+          </div>
+          <div className="admin-addproducts-file-categories-div">
+            <input
+              style={{ width: "250px" }}
+              type="number"
+              placeholder="Products Price"
+              required
+            />
+            <input
+              style={{ width: "250px", marginLeft: "50px" }}
+              type="number"
+              placeholder="Products Stock"
+              required
+            />
+          </div>
+
+          <input
+            style={{ width: "550px", height: "100px" }}
+            type="text"
+            placeholder="Product Description"
+            required
+          />
+          <br />
+          <input
+            style={{ width: "550px", marginBottom: "20px" }}
+            type="submit"
+            value="ADD PRODUCTS"
+          />
+        </div>
+      </form>
     </div>
   );
 };
--- src/admin/AdminCategories/AdminCategories.jsx
+++ src/admin/AdminCategories/AdminCategories.jsx
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AdminCategories.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const AdminCategories = () => {
+  const [showform, setShowForm] = useState(false);
   return (
     <div>
-      <h1>Categories</h1>
+      <h1 className="admin-addproductscategory-headline ">Categories</h1>
+      <div className="admin-addproductscategory-div">
+        <input
+          style={{ width: "550px", marginBottom: "20px" }}
+          type="submit"
+          value={showform ? "ADD CATEGORY" : "CLICK FOR CATEGORY FORM"}
+          onClick={() => setShowForm(true)}
+        />
+        {showform && (
+          <form onSubmit={() => setShowForm(true)} action="">
+            <input
+              style={{ width: "550px" }}
+              type="text"
+              placeholder="Products Categories"
+              required
+            />
+            <br />
+            <input
+              style={{ width: "550px" }}
+              type="text"
+              placeholder="Category Description"
+              required
+            />
+            <br />
+            <input
+              style={{ width: "550px", marginBottom: "20px" }}
+              type="submit"
+              value="SUBMIT CATEGORY"
+            />
+          </form>
+        )}
+      </div>
+      <h1 className="admin-addproductscategory-headline ">Category List</h1>
+
+      <table>
+        <tr>
+          <th>Category Name</th>
+          <th>Description</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>
+            <EditIcon
+              fontSize="large"
+              style={{
+                backgroundColor: "  rgb(61, 209, 61)",
+                color: "white",
+                padding: "5px",
+                cursor: "pointer",
+                borderRadius: "5px",
+                marginRight: "10px",
+              }}
+            />
+            <DeleteIcon
+              fontSize="large"
+              style={{
+                backgroundColor: "   rgb(238, 30, 30)",
+                color: "white",
+                padding: "5px",
+                cursor: "pointer",
+                borderRadius: "5px",
+              }}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+        <tr>
+          <td>Adam</td>
+          <td>Johnson</td>
+          <td>67</td>
+        </tr>
+      </table>
     </div>
   );
 };
